e704e19a07117741d0a02992a2680f4f9f1e71a3
Philip Diffenderfer <pdiffenderfer@gmail.com>
2019-10-23 00:12:04 -0400

    Fix build for pages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/expangine-vue/'
+    : '/',
   configureWebpack(config) {
     config.devtool = 'eval-source-map';
     config.module.rules.push({
