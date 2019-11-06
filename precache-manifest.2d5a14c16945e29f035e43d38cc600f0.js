37f5b158bce69150706bae4155118f7b60c5b988
Philip Diffenderfer <pdiffenderfer@gmail.com>
2019-11-06 10:37:20 -0500

    Made it a PWA

---
+++ precache-manifest.2d5a14c16945e29f035e43d38cc600f0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bf42327576e18887f5ca465216afa4c",
+    "url": "/expangine-vue/browserconfig.xml"
+  },
+  {
+    "revision": "03038dcd4ede654f189f",
+    "url": "/expangine-vue/css/app.e041b45c.css"
+  },
+  {
+    "revision": "9535519ae6526be1438f",
+    "url": "/expangine-vue/css/chunk-vendors.7320b25f.css"
+  },
+  {
+    "revision": "7f81c66d4ccd3fd33fd48074b8af1b30",
+    "url": "/expangine-vue/index.html"
+  },
+  {
+    "revision": "03038dcd4ede654f189f",
+    "url": "/expangine-vue/js/app.bc6952e1.js"
+  },
+  {
+    "revision": "9535519ae6526be1438f",
+    "url": "/expangine-vue/js/chunk-vendors.e61c096f.js"
+  },
+  {
+    "revision": "c44a8794755983d1c1716d18043ff0f9",
+    "url": "/expangine-vue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/expangine-vue/robots.txt"
+  }
+]);
