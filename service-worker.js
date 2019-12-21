bfb24b966c3fbca146a735196780f1ea8e01c132
Philip Diffenderfer <pdiffenderfer@gmail.com>
2019-12-21 11:51:03 -0500

    Set build with corrected set casting

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.776d1f78b999cfdad2ede2138be8e519.js"
+  "precache-manifest.9d8c64bea4383482b7237de6aafd68b6.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "expangine-vue"});
